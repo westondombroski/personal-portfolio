a313cc2aa689223c4bf75d45e14856b1c121e1e4
westondombroski <westondombroski@gmail.com>
2017-01-31 00:32:40 -0800

    worked on a bunch of js/jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,34 +60,35 @@ $(document).ready(function(){
 
 
 var projects = [{
-    'picture': "#",
+    'picture': "assets/imgs/code.jpg",
     'name': "weston dombroski portfolio project",
     'description': "the code of the site you are currently enjoying, enjoy",
     'link': "https://github.com/westondombroski/westondombroski.github.io"
   },
-  { 'picture': "#",
+  { 'picture': "assets/imgs/code.jpg",
     'name': "wdi memory game",
     'description': "a little four card memory game",
     'link':"https://github.com/westondombroski/wdi-fundamentals-memorygame"
   },
-  { 'picture': "#",
+  { 'picture': "assets/imgs/code.jpg",
     'name': "js basics",
     'description': "showing off only the most basic of js skills",
     'link': "https://github.com/westondombroski/problem-set-js-basics"
   },
-  { 'picture': "#",
+  { 'picture': "assets/imgs/code.jpg",
     'name': "building js iterators",
     'description': "just building some javascript iterators",
     'link': "https://github.com/westondombroski/building-js-iterators-lab"
   }];
 
-var getProjectInfo = function(){
-  for(var i = 0; i < projects.length; i++){
-  }
-}
+  for(var i = 0; i < projects.length; i++) {
+      var project = ("<li class='col-md-6'><a href='" +
+      projects.link[i] + "'><img src='" +
+      projects.picture[i] + "'alt='" +
+      projects.name[i] + "'class='img-rounded'><p>" +
+      projects.description[i] + "</p></a></li>");
+    }
 
+    $(".projects").append(project);
 
-
-
-
-});
+ });
